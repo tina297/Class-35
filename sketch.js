ae1523d4d5f8b37cdccc7c3162c1bbcb7f4c0d27
tina297 <74814611+tina297@users.noreply.github.com>
2021-02-06 09:27:06 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var ball;
+var position, database;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPosition = database.ref("ball/Position");
+    ballPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        wwritePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/Position").set({
+        'X':position.x + x,
+        'Y':position.y + y
+    })
+   
+}
+
+function readPosition(data) {
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+
+function showError() {
+    console.log("ERROR")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
